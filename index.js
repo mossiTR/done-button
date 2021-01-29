095a006cef7546e0a147dccdb8571ed990f42682
mossiTR <72362768+mossiTR@users.noreply.github.com>
2021-01-29 09:26:52 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const button = document.getElementById("add-button");
+button.addEventListener("click", () => {
+  const input = document.getElementById("new-todo");
+  console.log(input.value);
+
+  const li = document.createElement("li");
+  li.innerText = input.value;
+
+  const doneButton = document.createElement("button");
+  doneButton.innerText = "done";
+  li.appendChild(doneButton);
+  doneButton.addEventListener("click", () => {
+    const li = doneButton.closest("li");
+    li.classList.add("done");
+  });
+
+  const ul = document.querySelector("ul");
+  ul.appendChild(li);
+});
